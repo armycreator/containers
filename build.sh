7450b7f2bdb07c9d0cac10598861fcb2e9871d99
Julien Deniau <julien.deniau@gmail.com>
2017-03-16 17:49:28 +0100

    upgrade build script

--- build.sh
+++ build.sh
@@ -1,25 +1,50 @@
 #!/bin/bash
 
-WORKING_DIR="/var-www/armycreator"
+WORKING_DIR="/var/www/armycreator"
 VOLUME="$(pwd)/../armycreator-website:$WORKING_DIR"
-
-function init() {
-    docker-compose exec php5 php /usr/local/bin/composer install
-    # docker run -v "$VOLUME" -w $WORKING_DIR --rm composer:latest install
-    docker run -v "$VOLUME" -w $WORKING_DIR --rm node:6 npm install
-    docker run -v "$VOLUME" --rm node:6 "$WORKING_DIR/node_modules/.bin/bower install"
-    docker-compose exec php5 php app/console doctrine:migrations:migrate
-}
 
 function composer() {
     # docker run -v "$VOLUME" -w $WORKING_DIR --rm composer:latest composer $1
-    docker-compose exec php5 php /usr/local/bin/composer $1
+    docker-compose exec php5 php /usr/local/bin/composer $*
 }
 
-if [ $1 = "init" ]
+function symfony() {
+    docker-compose exec php5 php app/console $*
+}
+
+function bower() {
+    docker run -v "$VOLUME" -w $WORKING_DIR --rm node:6 "./node_modules/.bin/bower $*"
+}
+
+function npm() {
+    docker run -v "$VOLUME" -w $WORKING_DIR --rm node:6 npm $*
+}
+
+function init() {
+    composer install
+    # docker run -v "$VOLUME" -w $WORKING_DIR --rm composer:latest install
+    npm install
+    bower install
+    symfony doctrine:migrations:migrate  --no-interaction
+    # docker-compose exec php5 php app/console doctrine:migrations:migrate 
+}
+
+COMMAND=$1
+shift
+
+if [ $COMMAND = "init" ]
 then
     init
-elif [ $1 = "composer" ]
+elif [ $COMMAND = "composer" ]
 then
-    composer $2
+    composer $*
+elif [ $COMMAND = "sf" ]
+then
+    symfony $*
+elif [ $COMMAND = "npm" ]
+then
+    symfony $*
+elif [ $COMMAND = "bower" ]
+then
+    symfony $*
 fi
